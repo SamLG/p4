4b1b1657fa32185d4ecf063d3b853bc0cd040d47
Samantha Grise <AndrewMartin@AndrewMartin.local>
2016-12-09 17:37:55 -0500

    fix nametroubleshooting strange issue with certain pages not loading

--- resources/views/garden/index.blade.php
+++ resources/views/garden/index.blade.php
@@ -1,1 +1,30 @@
-<h1>This Works!</h1>
+@extends('layouts.master')
+
+
+@section('title')
+    Green Thumb Garden
+@stop
+
+
+{{--
+This `head` section will be yielded right before the closing </head> tag.
+Use it to add specific things that *this* View needs in the head,
+such as a page specific stylesheets.
+--}}
+@section('head')
+@stop
+
+
+@section('content')
+    <h2>Here is your garden!</h2>
+
+@stop
+
+
+{{--
+This `body` section will be yielded right before the closing </body> tag.
+Use it to add specific things that *this* View needs at the end of the body,
+such as a page specific JavaScript files.
+--}}
+@section('body')
+@stop
