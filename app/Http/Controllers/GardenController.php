720a1273590b863125ca03ceadeab7b3df858723
Samantha Grise <AndrewMartin@AndrewMartin.local>
2016-12-09 17:27:35 -0500

    fix nametroubleshooting strange issue with certain pages not loading

--- app/Http/Controllers/GardenController.php
+++ app/Http/Controllers/GardenController.php
@@ -21,50 +21,50 @@ class GardenController extends Controller
 
         // return view('garden.index');
     }
-    /**
-     * Responds to requests to GET /
-     * purpose: Show individual plant
-     * Route::get('/gardens/show/{id}', 'GardenController@show')->name('plants.show');
-     */
-    public function show($id)
-    {
-        $plant = Plant::find($id);
-        return view('plants.show')->with(['plant'=>$plant]);
-    }
-    /**
-     * Responds to requests to GET /
-     * purpose: Show form to add plant
-     * Route::post('/gardens/create', 'GardenController@create')->name('plants.create');
-     */
-    public function create()
-    {
-        return view('plants.create');
-    }
-    /**
-     * Responds to requests to POST /
-     * purpose: Process form to add plant
-     * Route::post('/gardens/create', 'GardenController@store')->name('plants.store');
-     */
-    public function store()
-    {
-        return view('plants.store');
-    }
-    /**
-     * Responds to requests to GET /
-     * purpose: Show form to edit plant
-     * Route::get('/gardens/edit/{id}}', 'GardenController@edit')->name('plants.edit');
-     */
-    public function edit()
-    {
-        return view('plants.edit');
-    }
-    /**
-     * Responds to requests to POST /
-     * purpose: Process form to edit plant
-     * Route::put('/gardens/edit/{id}', 'GardenController@update')->name('plants.update');
-     */
-    public function update()
-    {
-        return view('plants.update');
-    }
+    // /**
+    //  * Responds to requests to GET /
+    //  * purpose: Show individual plant
+    //  * Route::get('/gardens/show/{id}', 'GardenController@show')->name('plants.show');
+    //  */
+    // public function show($id)
+    // {
+    //     $plant = Plant::find($id);
+    //     return view('plants.show')->with(['plant'=>$plant]);
+    // }
+    // /**
+    //  * Responds to requests to GET /
+    //  * purpose: Show form to add plant
+    //  * Route::post('/gardens/create', 'GardenController@create')->name('plants.create');
+    //  */
+    // public function create()
+    // {
+    //     return view('plants.create');
+    // }
+    // /**
+    //  * Responds to requests to POST /
+    //  * purpose: Process form to add plant
+    //  * Route::post('/gardens/create', 'GardenController@store')->name('plants.store');
+    //  */
+    // public function store()
+    // {
+    //     return view('plants.store');
+    // }
+    // /**
+    //  * Responds to requests to GET /
+    //  * purpose: Show form to edit plant
+    //  * Route::get('/gardens/edit/{id}}', 'GardenController@edit')->name('plants.edit');
+    //  */
+    // public function edit()
+    // {
+    //     return view('plants.edit');
+    // }
+    // /**
+    //  * Responds to requests to POST /
+    //  * purpose: Process form to edit plant
+    //  * Route::put('/gardens/edit/{id}', 'GardenController@update')->name('plants.update');
+    //  */
+    // public function update()
+    // {
+    //     return view('plants.update');
+    // }
 } # end of class
